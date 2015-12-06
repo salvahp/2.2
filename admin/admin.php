717e3a9826fb2b55e0d87214b52bb1560da90b45
Dan Mortensen <enklopida@gmail.com>
2015-12-06 02:30:29 +0100

    CMS gemmer på een knap
    Mangler smårettelser i css og tekst

--- admin/admin.php
+++ admin/admin.php
@@ -27,402 +27,405 @@ $file = new FileSystem;
 		<h1>Rediger Menukort</h1>
 		<p>Her redigeres i Menukortet på Peter Gift hjemmesiden. <!-- Punkterne gemmes enkeltvis--></p>
 	</section>
-	<?php
-// Display success message
-if(isset($_GET["success"])) {
-	?>
-	<script>
-    alert("Siden er blevet opdateret");
-</script>
-	<?php
-}
-?>
 	<main id="menupunkter">
-		<section class="punkt">
-			<h2>Punkt 1</h2>
-			<form action="processes/process1.php" method="post">
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+		<form action="processes/process1.php" method="post">
+			<section class="formfelt">
+				<h2>Punkt 1</h2>
+				<h3>Titel</h3>
+				<textarea class="text" name="text1"><?php			
 				// Læs menupunkt
 				$filename = "includes/menukort/menukort_1.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description1" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_1_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price1"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_1_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-<!--				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process2.php" method="post">-->
-<h2>Punkt 2</h2>
-				<textarea class="text" name="text2" rows="2" cols="50"><?php			
+				kr. </section>
+			<section class="formfelt">
+				<h2>Punkt 2</h2>
+				<h3>Titel</h3>
+				<textarea class="text" name="text2" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_2.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description2" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description2" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_2_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price2" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price2"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_2_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process3.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 3</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text3" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_3.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description3" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_3_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price3"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_3_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process4.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 4</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text4" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_4.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description4" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_4_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price4"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_4_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process5.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 5</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text5" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_5.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description5" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_5_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price5"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_5_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process6.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 6</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text6" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_6.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description6" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_6_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price6"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_6_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process7.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 7</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text7" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_7.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description7" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_7_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price7"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_7_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process8.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 8</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text8" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_8.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description8" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_8_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price8"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_8_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process9.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 9</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text9" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_9.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description9" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_9_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price9"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_9_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process10.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 10</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text10" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_10.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description10" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_10_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price10"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_10_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process11.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 11</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text11" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_11.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description11" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_11_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price11"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_11_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process12.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 12</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text12" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_12.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description12" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_12_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price12"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_12_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process13.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 13</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text13" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_13.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description13" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_13_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price13"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_13_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process14.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 14</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text14" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_14.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description14" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_14_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price14"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_14_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process15.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 15</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text15" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_15.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description15" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_15_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price15"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_15_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
-		<section class="punkt">
-			<form action="processes/process16.php" method="post">
+			</section>
+			<section class="formfelt">
 				<h2>Punkt 16</h2>
-				<textarea class="text" name="text" rows="2" cols="50"><?php			
+				<h3>Titel</h3>
+				<textarea class="text" name="text16" ><?php			
 				// Read file
 				$filename = "includes/menukort/menukort_16.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="description" name="description" rows="2" cols="50" ><?php			
+				<h3>Beskrivelse</h3>
+				<textarea class="description" name="description16" ><?php			
 				// læs beskrivelse
 				$filename = "includes/menukort/menukort_16_beskrivelse.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<textarea class="price" name="price" rows="2" cols="5" ><?php			
+				<h3>Pris</h3>
+				<textarea class="price" name="price16"><?php			
 				// læs pris
 				$filename = "includes/menukort/menukort_16_pris.html";
 				echo($file->read($filename));
 			?>
 </textarea>
-				<button type="submit" class="button">Gem</button>
-			</form>
-		</section>
+			</section>
+			<button type="submit" class="button">Gem</button>
+		</form>
 	</main>
 </div>
+<?php
+// Display success message
+if(isset($_GET["success"])) {
+	?>
+<script>
+    alert("Siden er blevet opdateret");
+</script>
+<?php
+}
+?>
 </body>
 </html>
--- admin/processes/process1.php
+++ admin/processes/process1.php
@@ -6,17 +6,17 @@ $file = new FileSystem;
 
 // Write to file (silent)
 $filename = "../includes/menukort/menukort_1.html";
-$content = $_POST["text"];
+$content = $_POST["text1"];
 $file->write($filename,$content);
 
 // Write to file (silent)
 $filename = "../includes/menukort/menukort_1_pris.html";
-$content = $_POST["price"];
+$content = $_POST["price1"];
 $file->write($filename,$content);
 
 // Write to file (silent)
 $filename = "../includes/menukort/menukort_1_beskrivelse.html";
-$content = $_POST["description"];
+$content = $_POST["description1"];
 $file->write($filename,$content);
 
 //
@@ -24,76 +24,258 @@ $file->write($filename,$content);
 //
 // Write to file (silent)
 $filename = "../includes/menukort/menukort_2.html";
-$content = $_POST["text"];
+$content = $_POST["text2"];
 $file->write($filename,$content);
 
 // Write to file (silent)
 $filename = "../includes/menukort/menukort_2_pris.html";
-$content = $_POST["price"];
+$content = $_POST["price2"];
 $file->write($filename,$content);
 
 // Write to file (silent)
 $filename = "../includes/menukort/menukort_2_beskrivelse.html";
-$content = $_POST["description"];
+$content = $_POST["description2"];
 $file->write($filename,$content);
 
 //
 // Her kommer menupunkt 3
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_3.html";
+$content = $_POST["text3"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_3_pris.html";
+$content = $_POST["price3"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_3_beskrivelse.html";
+$content = $_POST["description3"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 4
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_4.html";
+$content = $_POST["text4"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_4_pris.html";
+$content = $_POST["price4"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_4_beskrivelse.html";
+$content = $_POST["description4"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 5
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_5.html";
+$content = $_POST["text5"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_5_pris.html";
+$content = $_POST["price5"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_5_beskrivelse.html";
+$content = $_POST["description5"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 6
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_6.html";
+$content = $_POST["text6"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_6_pris.html";
+$content = $_POST["price6"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_6_beskrivelse.html";
+$content = $_POST["description6"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 7
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_7.html";
+$content = $_POST["text7"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_7_pris.html";
+$content = $_POST["price7"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_7_beskrivelse.html";
+$content = $_POST["description7"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 8
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_8.html";
+$content = $_POST["text8"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_8_pris.html";
+$content = $_POST["price8"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_8_beskrivelse.html";
+$content = $_POST["description8"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 9
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_9.html";
+$content = $_POST["text9"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_9_pris.html";
+$content = $_POST["price9"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_9_beskrivelse.html";
+$content = $_POST["description9"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 10
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_10.html";
+$content = $_POST["text10"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_10_pris.html";
+$content = $_POST["price10"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_10_beskrivelse.html";
+$content = $_POST["description10"];
+$file->write($filename,$content);
 
 //
 // Her kommer menupunkt 11
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_11.html";
+$content = $_POST["text11"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_11_pris.html";
+$content = $_POST["price11"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_11_beskrivelse.html";
+$content = $_POST["description11"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 12
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_12.html";
+$content = $_POST["text12"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_12_pris.html";
+$content = $_POST["price12"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_12_beskrivelse.html";
+$content = $_POST["description12"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 13
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_13.html";
+$content = $_POST["text13"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_13_pris.html";
+$content = $_POST["price13"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_13_beskrivelse.html";
+$content = $_POST["description13"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 14
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_14.html";
+$content = $_POST["text14"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_14_pris.html";
+$content = $_POST["price14"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_14_beskrivelse.html";
+$content = $_POST["description14"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 15
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_15.html";
+$content = $_POST["text15"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_15_pris.html";
+$content = $_POST["price15"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_15_beskrivelse.html";
+$content = $_POST["description15"];
+$file->write($filename,$content);
 //
 // Her kommer menupunkt 16
 //
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_16.html";
+$content = $_POST["text16"];
+$file->write($filename,$content);
 
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_16_pris.html";
+$content = $_POST["price16"];
+$file->write($filename,$content);
+
+// Write to file (silent)
+$filename = "../includes/menukort/menukort_16_beskrivelse.html";
+$content = $_POST["description16"];
+$file->write($filename,$content);
 
 
 // Redirect to form
